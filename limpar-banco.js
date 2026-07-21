0028a68ec80f041ed624b9be3ad42da153678567
tharikluan <tharikluan.miuel@rede.ulbra.br>
2026-07-20 22:22:50 -0300

    corrige conexao SSL do limpar-banco.js

--- limpar-banco.js
+++ limpar-banco.js
@@ -2,12 +2,8 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false }
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 async function limpar() {
